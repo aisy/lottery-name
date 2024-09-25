dc28e81c220fb684a3787dadd95428ec597759b7
Aisy Rozsidhy <aisymuhammad@gmail.com>
2024-09-25 13:14:47 +0700

    first upload

---
+++ src/app/components/NavbarApp.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface INavbarAppProps {
+}
+
+const NavbarApp: React.FunctionComponent<INavbarAppProps> = (props) => {
+   return (
+      <div className='bg-white/80 top-0 p-4 rounded-b-2xl w-full'>
+         <div className='flex justify-between'>
+            <div>Satkomindo</div>
+            <div>Setting</div>
+         </div>
+      </div>
+   );
+};
+
+export default NavbarApp;
