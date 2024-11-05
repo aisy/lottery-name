1cb319482904f00d0abffdb9bf37a27f8e6fdc7e
Aisy Rozsidhy <aisymuhammad@gmail.com>
2024-11-05 17:34:29 +0700

    change Dialog Style

--- src/app/components/DialogWinner.tsx
+++ src/app/components/DialogWinner.tsx
@@ -1,26 +1,34 @@
 import * as React from 'react';
-import {Modal, ModalContent, ModalBody, ModalHeader, } from '@nextui-org/react'
+import {Modal, ModalContent, ModalBody, ModalHeader,} from '@nextui-org/react'
 
 interface IDialogWinnerProps {
-   openDialog: boolean
-   close: () => void
-   winner: string
+    openDialog: boolean
+    close: () => void
+    winner: string
 }
 
-const DialogWinner: React.FunctionComponent<IDialogWinnerProps> = ({ openDialog, close, winner }) => {
+const DialogWinner: React.FunctionComponent<IDialogWinnerProps> = ({openDialog, close, winner}) => {
 
-   return (
-       <Modal isOpen={openDialog} onClose={close}>
-         <ModalContent>
-            <>
-               <ModalHeader className="flex flex-col gap-1">Selamat!</ModalHeader>
-               <ModalBody>
-                  Pemenangnya adalah {winner}
-               </ModalBody>
-            </>
-         </ModalContent>
-       </Modal>
-   );
+    return (
+        <Modal isOpen={openDialog} onClose={close}>
+            <ModalContent>
+                <>
+                    <ModalHeader className="flex flex-col gap-1"></ModalHeader>
+                    <ModalBody>
+                        <div className={"text-center mb-10"}>
+                            <div className={"font-bold text-xl text-blue-600 uppercase"}>Selamat</div>
+                            <div className={"text-2xl"}>
+                                {winner}
+                            </div>
+                            <div className={"font-bold"}>
+                                mendapatkan hadiah
+                            </div>
+                        </div>
+                    </ModalBody>
+                </>
+            </ModalContent>
+        </Modal>
+    );
 };
 
 export default DialogWinner;
