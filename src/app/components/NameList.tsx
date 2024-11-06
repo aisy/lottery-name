8139ce3f107240bd1ef56d5cbeda5dc383131169
Aisy Rozsidhy <aisymuhammad@gmail.com>
2024-11-06 13:30:52 +0700

    fix useEffect and implement list

--- src/app/components/NameList.tsx
+++ src/app/components/NameList.tsx
@@ -1,66 +1,72 @@
-import { useEffect, useState, FunctionComponent } from 'react';
+import {useEffect, useState, FunctionComponent} from 'react';
 import useWinnerStore from '../stores/storeWinner';
+import useListLotery from "@/app/stores/storeListLotery";
 import DialogWinner from './DialogWinner';
 
 interface INameListProps {
-   startShuffle: boolean
-   stopShuffle: () => void
+    startShuffle: boolean
+    stopShuffle: () => void
 }
 
-const NameList: FunctionComponent<INameListProps> = ({ startShuffle, stopShuffle }) => {
+const NameList: FunctionComponent<INameListProps> = ({startShuffle, stopShuffle}) => {
 
-   const shuffleName = (array: string[]) => {
-      const randomIndex = Math.floor(Math.random() * array.length);
-      return array[randomIndex];
-   }
+    const shuffleName = (array: string[]) => {
+        const randomIndex: number = Math.floor(Math.random() * array.length);
+        return array[randomIndex];
+    }
 
-   const listName = ["Dio", "Jojo", "Joseph", "Donald", "Jodio"];
-   const { winner, setWinner } = useWinnerStore();
-   const [isOpen, setIsOpen] = useState(false);
+    // set list
+    const {listLotery, setListLotery} = useListLotery()
+    const {winner, setWinner} = useWinnerStore();
+    const [isOpen, setIsOpen] = useState(false);
 
-   // function open() {
-   //    setIsOpen(true)
-   // }
+    function close() {
+        setIsOpen(false)
+    }
 
-   function close() {
-      setIsOpen(false)
-   }
+    // set
+    useEffect(() => {
+        setListLotery(["Dio", "Jojo", "Joseph", "Donald", "Jodio"]);
+    }, [setListLotery]);
 
-   useEffect(() => {
-      // Inisialisasi winner di sisi klien setelah komponen dirender
-      // const shuffleTime = 
-      setWinner(shuffleName(listName));
-   }, []);
+    useEffect(() => {
+        setWinner(shuffleName(listLotery));
+    }, [listLotery, setWinner]);
 
-   useEffect(() => {
-      if (startShuffle) {
-         const shuffleInterval = setInterval(() => {
-            setWinner(shuffleName(listName));
-         }, 80);
+    useEffect(() => {
+        if (startShuffle) {
+            const shuffleInterval = setInterval(() => {
+                setWinner(shuffleName(listLotery));
+            }, 80);
 
-         setTimeout(() => {
-            clearInterval(shuffleInterval);
-            setWinner(shuffleName(listName));
-            setIsOpen(true)
-            stopShuffle()
-         }, 5000); // Animasi berlangsung selama 5 detik
+            setTimeout(() => {
+                clearInterval(shuffleInterval);
 
-         return () => clearInterval(shuffleInterval);
-      }
-   }, [startShuffle]);
+                // set winner
+                setWinner(shuffleName(listLotery));
+                // open dialog
+                setIsOpen(true)
 
-   return (
-      <div className={'w-full'}>
-         <DialogWinner openDialog={isOpen} close={close} winner={winner} />
-         <div className={'flex items-center justify-center'}>
-            <div className='flex bg-white w-3/4 px-4 py-2 rounded-full justify-center items-center'>
-               <div className="font-bold text-4xl">
-                  {winner}
-               </div>
+                // stop shuffle
+                stopShuffle()
+            }, 5000); // set animation 5 detik
+
+            return () => clearInterval(shuffleInterval);
+        }
+    }, [startShuffle, listLotery, setWinner, stopShuffle]);
+
+    return (
+        <div className={'w-full'}>
+            <DialogWinner openDialog={isOpen} close={close} winner={winner}/>
+            <div className={'flex items-center justify-center'}>
+                <div className='flex bg-white w-3/4 px-4 py-2 rounded-full justify-center items-center'>
+                    <div className="font-bold text-4xl">
+                        {winner}
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   );
+        </div>
+    );
 };
 
 export default NameList;
