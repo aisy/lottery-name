591a3ab36a16ab1e12c448a0d4aa103188ee4ec8
Aisy Rozsidhy <aisymuhammad@gmail.com>
2024-11-14 13:18:00 +0700

    add link

--- src/components/NavbarApp.tsx
+++ src/components/NavbarApp.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
+import Link from 'next/link'
 
 interface INavbarAppProps {
 }
 
 const NavbarApp: React.FunctionComponent<INavbarAppProps> = (props) => {
    return (
-      <div className='bg-white/80 top-0 p-4 rounded-b-2xl w-full'>
+      <div className='bg-white backdrop-filter backdrop-blur-lg bg-opacity-30 top-0 sticky p-4 border-b border-gray-200 rounded-b-2xl min-w-full'>
          <div className='flex justify-between'>
-            <div>Satkomindo</div>
-            <div>Settings</div>
+            <div>
+               <Link href={'/'} className='font-bold text-xl'>Lottery</Link>
+            </div>
+            <div>
+               <Link href={'/settings'}>Settings</Link>
+            </div>
          </div>
       </div>
    );
