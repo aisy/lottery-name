045b07de4610fda03352b4a14041342732e703e0
Aisy Rozsidhy <aisymuhammad@gmail.com>
2024-11-30 13:23:42 +0700

    add persist

--- src/stores/storeWinner.tsx
+++ src/stores/storeWinner.tsx
@@ -1,20 +1,57 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 // state
 interface WinnerState {
     winner: string
+}
+interface WinnerPersistState {
+    listWinner: string[]
 }
 
 // action
 interface WinnerActions {
     setWinner: (newWinner: string) => void
 }
+interface WinnerPersistAction {
+    setListWinner: (newListWinner: string) => void
+}
+
+
 
 type WinnerStore = WinnerState & WinnerActions
+type WinnerPersistStore = WinnerPersistState & WinnerPersistAction
 
-const useWinnerStore = create<WinnerStore>((set) => ({
-    winner: "",
-    setWinner: (newWinner: string) => set({winner: newWinner})
-}));
+const winnerStore = create<WinnerStore>((set) => (
+    {
+        winner: "",
+        setWinner: (newWinner: string) => {
+            set({ winner: newWinner })
+        },
+    }
+));
 
-export default useWinnerStore
+const winnnerPersistStore = create<WinnerPersistStore>()(
+    persist(
+        (set) => ({
+            listWinner: [],
+            setListWinner: (newListWinner: string) => {
+                // console.log(newListWinner);
+                set((state) => ({
+                    listWinner: [...state.listWinner, newListWinner],
+                }))
+            }
+        }),
+        {
+            // set name key in localstorage
+            name: 'listWinner',
+        }
+    )
+)
+
+const useStoreWinner = () => ({
+    ...winnerStore(),
+    ...winnnerPersistStore()
+})
+
+export default useStoreWinner
