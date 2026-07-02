ef173b5031fa60c1ad7bed2f68e009f96e5b32e2
aisy <aisymuhammad@gmail.com>
2026-07-02 18:00:47 +0700

    fix layout sidebar

--- src/components/DrawerOption.tsx
+++ src/components/DrawerOption.tsx
@@ -1,142 +1,103 @@
-'use client';
-import * as React from 'react';
-import { Drawer, DrawerClose, DrawerContent, DrawerHeader, DrawerOverlay, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
-import { FormikHelpers } from 'formik';
-import storeListLotery from '@/stores/storeListLottery';
-import { convertStringArray } from '@/lib/utils';
-import FormInput from './form/FormInput';
-import FormWrapper from './form/FormWrapper';
-import SubmitForm from './form/SubmitForm';
-
-// interface IDrawerOptionProps {
-// }
+"use client";
+import * as React from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+import { FormikHelpers } from "formik";
+import storeListLotery from "@/stores/storeListLottery";
+import { convertStringArray } from "@/lib/utils";
+import FormInput from "./form/FormInput";
+import FormWrapper from "./form/FormWrapper";
+import SubmitForm from "./form/SubmitForm";
+import { Settings2Icon } from "lucide-react";
 
 interface settingValues {
-  nameEvent: string
-  list: string
+  nameEvent: string;
+  list: string;
 }
 
 const DrawerOption: React.FunctionComponent = () => {
-
-  const { setListLottery, listLottery } = storeListLotery()
+  const { setListLottery, listLottery } = storeListLotery();
 
   const initialValues: settingValues = {
-    nameEvent: '',
-    list: listLottery.length > 0 ? listLottery.join(', ') : ''
-  }
+    nameEvent: "",
+    list: listLottery.length > 0 ? listLottery.join(", ") : "",
+  };
 
-  // // set formik form
-  // const formik = useFormik({
-  //   initialValues: {
-  //     nameEvent: '',
-  //     list: listLottery.length > 0 ? listLottery.join(', ') : ''
-  //   },
-  //   enableReinitialize: true,
-  //   onSubmit: (values: settingValues) => {
-  //     const convertArray = convertStringArray(values.list);
-  //     // const names = convertArray.join(',\n ');
-  //     // console.log(convertArray);
-  //     setListLottery(convertArray)
-  //   }
-  // })
-
-
-
-  const handleSubmit = async (values: settingValues, { setSubmitting }: FormikHelpers<settingValues>) => {
+  const handleSubmit = async (
+    values: settingValues,
+    { setSubmitting }: FormikHelpers<settingValues>,
+  ) => {
     await new Promise((resolve) => setTimeout(resolve, 500));
     const convertArray = convertStringArray(values.list);
-    // const names = convertArray.join(',\n ');
-    // console.log(convertArray);
-    setListLottery(convertArray)
+    setListLottery(convertArray);
     setSubmitting(false);
-    // formik.resetForm();
-  }
+  };
 
   return (
     <div>
-      <Drawer direction='right'>
+      <Drawer direction="right">
         {/* trigger button */}
         <DrawerTrigger>
-          <AdjustmentsHorizontalIcon className='size-7 bg-blue-200 p-1 rounded-md hover:bg-orange-300 hover:text-white' />
+          <AdjustmentsHorizontalIcon className="size-7 bg-blue-200 p-1 rounded-md hover:bg-orange-300 hover:text-white" />
         </DrawerTrigger>
 
         {/* overlay */}
         <DrawerOverlay>
           <DrawerContent>
             <DrawerHeader>
-              <DrawerTitle>Settings - Data</DrawerTitle>
+              <DrawerTitle>
+                <div className="flex items-center gap-2">
+                  <Settings2Icon className="size-4" />
+                  <div className="text-xl font-bold">Settings - Data</div>
+                </div>
+              </DrawerTitle>
               <DrawerClose />
             </DrawerHeader>
-            {/* <form onSubmit={formik.handleSubmit} className='flex flex-col space-y-5'>
-              <div className='p-5 flex flex-col space-y-5'>
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                  <Label htmlFor="nameEvent">Nama Event</Label>
-                  <Input
-                    id="nameEvent"
-                    name="nameEvent"
-                    type="text"
-                    placeholder='Nama event/acara'
-                    onChange={formik.handleChange}
-                    value={formik.values.nameEvent}
-                  />
-                </div>
-
-                <div className="grid w-full max-w-sm items-center gap-1.5">
-                  <Label htmlFor="list">List Data yang di Acak</Label>
-                  <Textarea
-                    id={'list'}
-                    name={'list'}
-                    placeholder='List data yang diacak'
-                    onChange={formik.handleChange}
-                    value={formik.values.list}
-                  />
-                </div>
-                <Button
-                  className='w-full rounded-full'
-                  type='submit'
-                  color='primary'
-                >
-                  Simpan Data
-                </Button>
-              </div>
-            </form> */}
-            <FormWrapper
-              initialValues={initialValues}
-              onSubmit={handleSubmit}
-              enableReinitialize={true}
-            >
-              {(formikProps) => (
-                <>
-                  <FormInput
-                    label='Nama Event'
-                    name='nameEvent'
-                    placeholder='Nama event/acara'
-                    type='text'
-                    as='input'
-                  />
-                  <FormInput
-                    label='List Data yang di Acak'
-                    name='list'
-                    placeholder='List data yang diacak'
-                    as='textarea'
-                    rows={6}
-                  />
-                  <SubmitForm
-                    className='w-full rounded-full'
-                    isSubmitting={formikProps.isSubmitting}
-                  >
-                    Simpan Data
-                  </SubmitForm>
-                </>
-              )}
-
-            </FormWrapper>
+            <div className="p-3">
+              <FormWrapper
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                enableReinitialize={true}
+              >
+                {(formikProps) => (
+                  <>
+                    <FormInput
+                      label="Nama Event"
+                      name="nameEvent"
+                      placeholder="Nama event/acara"
+                      type="text"
+                      as="input"
+                    />
+                    <FormInput
+                      label="List Data yang di Acak"
+                      name="list"
+                      placeholder="List data yang diacak"
+                      as="textarea"
+                      rows={6}
+                    />
+                    <SubmitForm
+                      className="w-full bg-blue-500 p-2 text-white rounded-full"
+                      isSubmitting={formikProps.isSubmitting}
+                    >
+                      Simpan Data
+                    </SubmitForm>
+                  </>
+                )}
+              </FormWrapper>
+            </div>
           </DrawerContent>
         </DrawerOverlay>
       </Drawer>
     </div>
-  )
+  );
 };
 
 export default DrawerOption;
--- src/components/form/FormInput.tsx
+++ src/components/form/FormInput.tsx
@@ -1,51 +1,50 @@
-import * as React from 'react';
-import { FormInputProps } from '@/types/form';
-import { Field, FieldProps } from 'formik';
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
+import * as React from "react";
+import { FormInputProps } from "@/types/form";
+import { Field, FieldProps } from "formik";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Textarea } from "../ui/textarea";
 
 const FormInput: React.FunctionComponent<FormInputProps> = ({
   label,
   name,
-  type = 'text',
+  type = "text",
   placeholder,
-  as = 'input',
+  as = "input",
   options,
-  rows = 3
+  rows = 3,
 }) => {
-
   return (
     <div>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
+      <div className="grid w-full max-w-sm items-center gap-1.5 my-5">
         <Label htmlFor={label}>{label}</Label>
         <Field name={name}>
           {({ field, meta }: FieldProps) => (
             <div>
-              {
-                as === 'textarea' ? (
-                  <textarea
-                    {...field}
-                    id={name}
-                    placeholder={placeholder}
-                    rows={rows}
-                  />
-                ) : as === 'select' && options ? (
-                  <select {...field} id={name} >
-                    <option value="">{placeholder || 'Select an option'}</option>
-                    {options.map((option) => (
-                      <option key={option.value} value={option.value}>
-                        {option.label}
-                      </option>
-                    ))}
-                  </select>
-                ) : (
-                  <Input
-                    {...field}
-                    id={name}
-                    type={type}
-                    placeholder={placeholder}
-                  />
-                )}
+              {as === "textarea" ? (
+                <Textarea
+                  {...field}
+                  id={name}
+                  placeholder={placeholder}
+                  rows={rows}
+                />
+              ) : as === "select" && options ? (
+                <select {...field} id={name}>
+                  <option value="">{placeholder || "Select an option"}</option>
+                  {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              ) : (
+                <Input
+                  {...field}
+                  id={name}
+                  type={type}
+                  placeholder={placeholder}
+                />
+              )}
               {meta.touched && meta.error && (
                 <div className="text-red-500 text-sm mt-1">{meta.error}</div>
               )}
