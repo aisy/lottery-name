636944ffb572172d747394363a6923c5ffe1807c
aisy <aisymuhammad@gmail.com>
2026-07-02 18:01:26 +0700

    fixing style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,32 @@
-"use client"
+"use client";
 import { useState } from "react";
 import NameList from "@/components/NameList";
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 import TitleLotery from "@/components/TitleLotery";
 import useListLotery from "@/stores/storeListLottery";
 
 export default function Home() {
-
-  const { listLottery } = useListLotery()
+  const { listLottery } = useListLotery();
   const [isShuffling, setIsShuffling] = useState<boolean>(false);
 
   const onClick = () => {
     setIsShuffling(true);
-  }
+  };
 
   const stopShuffle = () => {
     setIsShuffling(false);
-  }
+  };
 
   return (
     <div className="flex flex-col justify-center items-center h-fit">
-      <TitleLotery
-        title={"Event Ulang Tahung Satkomindo 2024"}
-      />
-      <NameList
-        startShuffle={isShuffling}
-        stopShuffle={stopShuffle}
-      />
+      <TitleLotery title={"Sistem Acak"} />
+      <NameList startShuffle={isShuffling} stopShuffle={stopShuffle} />
       <div className="mt-10">
         <Button
           disabled={listLottery.length === 0}
           size={"lg"}
+          variant={"default"}
           onClick={onClick}
-          className="bg-orange-400 px-3 py-2 text-white"
         >
           Pilih Pemenang
         </Button>
